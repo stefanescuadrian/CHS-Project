c191bfecf6e02e0e93a996264c415f9d006ef1a9
stefanescuadrian <stefanescuadrian04@gmail.com>
2021-12-20 16:27:12 +0200

    Hide Title of App from UI MainActivity.java

--- app/src/main/java/com/upt/cti/photogmap/MainActivity.java
+++ app/src/main/java/com/upt/cti/photogmap/MainActivity.java
@@ -2,9 +2,13 @@ package com.upt.cti.photogmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     public void logout(View view) {
